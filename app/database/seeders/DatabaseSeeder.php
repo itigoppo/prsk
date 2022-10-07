13009f8bbe4eae1ca72aa7a40721a2f648d94eb1
itigoppo <itigoppo+github@gmail.com>
2022-10-07 19:08:38 +0900

    デフォルトシード整え

--- app/database/seeders/DatabaseSeeder.php
+++ app/database/seeders/DatabaseSeeder.php
@@ -19,12 +19,17 @@ class DatabaseSeeder extends Seeder
         $this->call(MembersSeeder::class);
 //        $this->call(InteractionsSeeder::class);
 //        $this->call(ChangeLogsSeeder::class);
-        $this->call(TunesSeeder::class);
-        $this->call(DancersSeeder::class);
-        $this->call(SingersSeeder::class);
+
+//        $this->call(TunesSeeder::class);
+//        $this->call(DancersSeeder::class);
+//        $this->call(SingersSeeder::class);
+
         $this->call(CardsSeeder::class);
         $this->call(EventsSeeder::class);
         $this->call(EventCardsSeeder::class);
         $this->call(EventMembersSeeder::class);
+        $this->call(VirtualLivesSeeder::class);
+        $this->call(VirtualLiveMembersSeeder::class);
+        $this->call(VirtualLiveTunesSeeder::class);
     }
 }

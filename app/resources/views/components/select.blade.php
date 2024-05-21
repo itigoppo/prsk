ad0a6003ffb254f53cd5c4d5ee2d25a4a487c7a8
itigoppo <itigoppo+github@gmail.com>
2024-05-21 15:57:51 +0900

    フォームパーツデザイン調整

--- app/resources/views/components/select.blade.php
+++ app/resources/views/components/select.blade.php
@@ -7,8 +7,8 @@
 
 <span class="@twMerge('relative inline-flex' . ($class ? ' ' . $class : ''))">
   <select {{ $attributes }} @class([
-      'flex-auto appearance-none rounded border border-gray-400 py-[11px] pl-4 pr-10 text-sm leading-none shadow-[0_1px_3px_rgba(0,_0,_0,_0.24)] focus:border-puerto-rico-300 focus:ring-puerto-rico-300',
-      'pointer-events-none bg-gray-20 shadow-none' => $disabled,
+      'flex-auto appearance-none rounded border border-gray-300 py-[11px] pl-4 pr-10 text-sm leading-none shadow-sm hover:cursor-pointer focus:cursor-pointer focus:border-puerto-rico-300 focus:ring-puerto-rico-300',
+      'pointer-events-none bg-gray-400 text-white shadow-none' => $disabled,
   ])>
     @foreach ($options as $item)
       <option value="{{ $item['value'] }}" @selected($current === $item['value'])>{{ $item['label'] }}</option>
--- app/resources/views/components/text-input.blade.php
+++ app/resources/views/components/text-input.blade.php
@@ -1,6 +1,6 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
+<input @disabled($disabled) {!! $attributes->merge([
     'class' =>
-        'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-puerto-rico-300 dark:focus:border-puerto-rico-600 focus:ring-puerto-rico-300 dark:focus:ring-puerto-rico-600 rounded-md shadow-sm',
+        'border border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-puerto-rico-300 dark:focus:border-puerto-rico-600 focus:ring-puerto-rico-300 dark:focus:ring-puerto-rico-600 rounded-md shadow-sm disabled:bg-gray-400 disabled:text-white',
 ]) !!}>
